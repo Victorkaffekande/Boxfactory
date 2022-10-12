1b365f0c9435a37b311cf8c4da266f7e054afd83
Victorkaffekande <90904666+Victorkaffekande@users.noreply.github.com>
2022-10-12 13:22:44 +0200

    BeBox setup
    Co-Authored-By: magn20 <71693106+magn20@users.noreply.github.com>

---
+++ Domain/Enteties/Box.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Enteties;
+
+public class Box
+{
+    public String Name { get; set; }
+    public String Color { get; set; }
+    public int Id { get; set; }
+    public int Thickness { get; set; }
+    public int Width { get; set; }
+    public int Depth { get; set; }
+    public int Height { get; set; }
+    public double TotalVolume { get; set; }
+}
